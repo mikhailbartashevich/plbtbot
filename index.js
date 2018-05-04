9b3592481682ecea2840e3f2e18d9fb1ee08efd2
Mikhail <mihailbarashevich@gmail.com>
2018-05-04 12:13:15 +0200

    fixed dep

--- index.js
+++ index.js
@@ -84,3 +84,10 @@ bot.on(['/isNishchebrod'], function (msg) {
 
 // Start getting updates
 bot.start();
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(process.env.PORT || 3000);
