0995a6b0649bbf07a8b8534b1f6cb4c47653b474
Mikhail <mihailbarashevich@gmail.com>
2018-08-16 15:28:26 +0200

    added remont api

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ function checkRemontPrice(id) {
   1ZXASrDKPS2oF-UkrydC2N6khMuyJnanRctiEqu1wvEw/values/H16?key=AIzaSyBT95iNZMJphiiXzbKUTffs8T3TFVwf8XM`;
 
   request({url: renovationURL, json: true}, function(err, res, json) {
-    const total = json.data.values[0][0];
+    const total = json.values[0][0];
     bot.sendMessage(id, `Yura the Rich spent already for renovation: ${total}`);
   });
 
