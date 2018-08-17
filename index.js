296429d469e36a69dd30a7886271ffe43a9aa3f7
Mikhail <mihailbarashevich@gmail.com>
2018-08-17 10:45:27 +0200

    added save api

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const request = require('request');
 const http = require('http');
 const bot = new TeleBot('538233729:AAHJqMW2om913dzeTOECVxUm3Nb6AMTy7Xo');
 
+const fs = require('fs');
+const readline = require('readline');
+const { google } = require('googleapis');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = [
+  'https://www.googleapis.com/auth/drive',
+  'https://www.googleapis.com/auth/drive.file',
+  'https://www.googleapis.com/auth/spreadsheets'
+];
+const TOKEN_PATH = 'token.json';
+
 let interval = 0;
 let intervalTime = 0;
 
@@ -219,14 +231,13 @@ bot.on(['text'], function (msg) {
 });
 
 function rememberTweet(id, text, from) {
-  const tweetURL = `https://sheets.googleapis.com/v4/spreadsheets/1zH0oBaRmZxAJFtRnnMTTZk81kwTO_nTslLeVNDA8Ysw/values/A1:append?valueInputOption=RAW&key=AIzaSyBT95iNZMJphiiXzbKUTffs8T3TFVwf8XM`;
-
-  request.post({ url: tweetURL, form: { values: [[text + ' ' + from.first_name]] } }, 
-  function (err, httpResponse, body) {
-    console.log(JSON.stringify(err));
-    console.log(JSON.stringify(body));
-  })
-  bot.sendMessage(id, `Remembered`);
+  // Load client secrets from a local file.
+  fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Sheets API.
+    authorize(JSON.parse(content), appendTweet, text);
+  });
+  
 }
 
 // Start getting updates
@@ -237,3 +248,86 @@ http.createServer(function (req, res) {
   res.write('Welcome to @PolybiusBot. It is a Telegram bot. To use it open Telegram app.');
   res.end();
 }).listen(process.env.PORT || 3000);
+
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback, data) {
+  const { client_secret, client_id, redirect_uris } = credentials;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    try {
+      JSON.parse(token)
+    } catch (errr) {
+      return getNewToken(oAuth2Client, callback);
+    }
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client, data);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error while trying to retrieve access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function appendTweet(auth, text) {
+  var request = {
+    spreadsheetId: '1zH0oBaRmZxAJFtRnnMTTZk81kwTO_nTslLeVNDA8Ysw',  // TODO: Update placeholder value.
+    range: 'A1',
+    valueInputOption: 'RAW',  // TODO: Update placeholder value.
+    resource: {
+      values: [[
+        text
+      ]]
+    },
+    auth,
+  };
+  const sheets = google.sheets({version: 'v4', auth});
+  sheets.spreadsheets.values.append(request, function (err, response) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    bot.sendMessage(id, `Remembered`);
+  });
+}
