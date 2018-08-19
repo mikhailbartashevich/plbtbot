521596f5e1f2527ffc82cd684b955dc790a03677
Mikhail <mihailbarashevich@gmail.com>
2018-08-19 15:23:03 +0200

    added kartoshka

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ bot.on(['text'], function (msg) {
   if (msg.text.indexOf('запомните этот твит') > -1) {
     rememberTweet(id, msg);
   }
-  if (msg.text.indexOf('картош') > -1 || msg.text.indexOf('картоф') > -1 || msg.text.indexOf('бульб') > -1) {
+  if (msg.text.indexOf('картош') > -1 || msg.text.indexOf('картоф') > -1 || msg.text.indexOf('бульб') > -1 || msg.text.indexOf('картох') > -1 ) {
     kartoshkaJoke(id, msg);
   }
 });
