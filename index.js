3ecfed9097cde6be94b6206a1325fc5913094677
Mikhail <mihailbarashevich@gmail.com>
2018-11-17 15:14:23 +0100

    added split

--- index.js
+++ index.js
@@ -252,10 +252,12 @@ const splitwiseAPI = Splitwise({
 
 function splitwise(amount) {
   splitwiseAPI.createExpense({
-    users: [
-      // { user_id: '6181181' }, //m
-      { user_id: '6181165' } //p
-    ],
+    // users: [
+    //   // { user_id: '6181181' }, //m
+    //   { user_id: '6181165' } //p
+    // ],
+    users__0__user_id: '6181181',
+    users__1__user_id: '6181165',
     group_id: '5626964',
     currency_code: 'PLN',
     cost: amount,
