91e77d6baeef1361c68d43b35fbf30ebd5b43403
Mikhail <mihailbarashevich@gmail.com>
2018-11-19 20:16:50 +0100

    fixed currency

--- index.js
+++ index.js
@@ -343,55 +343,55 @@ bot.on(['text'], function (msg) {
   if (text.indexOf('ебучие джуны') > -1 || text.indexOf('ебучий джун') > -1) {
     eJunior(id, msg)
   }
-});
+})
 
 function getDebtCost (cost) {
   if (!cost) return 0
-  return parseFloat(cost, 10);
+  return parseFloat(cost, 10)
 }
 
 bot.on(['/split'], function (msg) {
-  const arr = msg.text.split(' ');
-  arr.shift(); // command
-  const cost = getDebtCost(arr.shift());
+  const arr = msg.text.split(' ')
+  arr.shift() // command
+  const cost = getDebtCost(arr.shift())
   const users = {
     437814936: { user_id: '6181181', owed_share: cost / 2 },
     281548620: { user_id: '6181165', owed_share: cost / 2 }
-  };
-  const foundUser = users[msg.from.id];
-  if (foundUser) {
-    foundUser.paid_share = cost;
-    splitwiseCreate(msg, [users[437814936], users[281548620]]);
   }
-});
+  const foundUser = users[msg.from.id]
+  if (foundUser) {
+    foundUser.paid_share = cost
+    splitwiseCreate(msg, [users[437814936], users[281548620]])
+  }
+})
 
 bot.on(['/debt'], function (msg) {
-  const arr = msg.text.split(' ');
-  arr.shift(); // command
-  const cost = getDebtCost(arr.shift());
+  const arr = msg.text.split(' ')
+  arr.shift() // command
+  const cost = getDebtCost(arr.shift())
   const users = {
     437814936: { user_id: '6181181', owed_share: cost },
     281548620: { user_id: '6181165', owed_share: cost }
-  };
-  const foundUser = users[msg.from.id];
-  if (foundUser) {
-    foundUser.paid_share = cost;
-    foundUser.owed_share = 0;
-    splitwiseCreate(msg, [users[437814936], users[281548620]]);
   }
-});
+  const foundUser = users[msg.from.id]
+  if (foundUser) {
+    foundUser.paid_share = cost
+    foundUser.owed_share = 0
+    splitwiseCreate(msg, [users[437814936], users[281548620]])
+  }
+})
 
-const Splitwise = require('splitwise');
+const Splitwise = require('splitwise')
 const splitwiseAPI = Splitwise({
   consumerKey: process.env.SPLITWISE_CONSUMER_KEY,
   consumerSecret: process.env.SPLITWISE_SECRET
-});
+})
 
 function splitwiseCreate (msg, users) {
-  const arr = msg.text.split(' ');
-  arr.shift(); // command
-  const stringCost = arr.shift();
-  const cost = getDebtCost(stringCost);
+  const arr = msg.text.split(' ')
+  arr.shift() // command
+  const stringCost = arr.shift()
+  const cost = getDebtCost(stringCost)
 
   splitwiseAPI
     .createExpense({
@@ -610,3 +610,17 @@ function processMessage (
     })
   })
 }
+
+const paralleldots = require('paralleldots');
+paralleldots.apiKey = process.env.PARALLEL_DOTS_KEY;
+
+bot.on(['/tokens'], function (msg) {
+  paralleldots
+    .multilangKeywords(msg.text, 'ru')
+    .then(response => {
+      bot.sendMessage(msg.chat.id, response)
+    })
+    .catch(error => {
+      console.log(error)
+    });
+})
