fdcbc587818aba23e84ddb6cf53c5369b1195cf9
Mikhail <mihailbarashevich@gmail.com>
2018-11-26 09:39:56 +0100

    refactoring

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -13,7 +13,7 @@ const SCOPES = [
 const TOKEN = {
   access_token: process.env.SPREADSHEET_ACCESS_TOKEN,
   token_type: "Bearer",
-  refresh_token: SPREADSHEET_REFRESH_TOKEN,
+  refresh_token: process.env.SPREADSHEET_REFRESH_TOKEN,
   scope: SCOPES.join(' '),
   expiry_date: 1534498917009
 }
@@ -35,42 +35,37 @@ function authorize (credentials, callback, data) {
   callback(oAuth2Client, data)
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getNewToken (oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  })
-  console.log('Authorize this app by visiting this url:', authUrl)
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-  rl.question('Enter the code from that page here: ', code => {
-    rl.close()
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) {
-        return console.error('Error while trying to retrieve access token', err)
-      }
-      oAuth2Client.setCredentials(token)
-      // Store the token to disk for later program executions
-      fs.writeFile(
-        path.resolve(__dirname, TOKEN_PATH),
-        JSON.stringify(token),
-        err => {
-          if (err) console.error(err)
-          console.log('Token stored to', path.resolve(__dirname, TOKEN_PATH))
-        }
-      )
-      callback(oAuth2Client)
-    })
-  })
-}
+
+// function getNewToken (oAuth2Client, callback) {
+//   const authUrl = oAuth2Client.generateAuthUrl({
+//     access_type: 'offline',
+//     scope: SCOPES
+//   })
+//   console.log('Authorize this app by visiting this url:', authUrl)
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   })
+//   rl.question('Enter the code from that page here: ', code => {
+//     rl.close()
+//     oAuth2Client.getToken(code, (err, token) => {
+//       if (err) {
+//         return console.error('Error while trying to retrieve access token', err)
+//       }
+//       oAuth2Client.setCredentials(token)
+//       // Store the token to disk for later program executions
+//       fs.writeFile(
+//         path.resolve(__dirname, TOKEN_PATH),
+//         JSON.stringify(token),
+//         err => {
+//           if (err) console.error(err)
+//           console.log('Token stored to', path.resolve(__dirname, TOKEN_PATH))
+//         }
+//       )
+//       callback(oAuth2Client)
+//     })
+//   })
+// }
 
 module.exports = {
   authorize
