3f78839e59f5fd9c5e1088d90a4bcaa50f4dea16
Mikhail <mihailbarashevich@gmail.com>
2019-02-25 20:23:19 +0100

    mimimi

--- src/text.js
+++ src/text.js
@@ -18,6 +18,7 @@ function updateMessages (messages, text) {
 }
 
 function textProcessing (msg, teleBot, collectedMessages) {
+  const id = msg.chat.id;
   const text = msg.text.toLowerCase()
   const tweet = new Tweet({
     user: msg.from.first_name + ' ' + msg.from.last_name,
@@ -27,6 +28,15 @@ function textProcessing (msg, teleBot, collectedMessages) {
   tweet.save().then(() => console.log('meow'));
 
   updateMessages(collectedMessages, text)
+
+  if (text.indexOf('мимими') > -1) {
+    const url = 'http://img1.joyreactor.cc/pics/post/%D0%BF%D0%B5%D1%81%D0%BE%D1%87%D0%BD%D0%B8%D1%86%D0%B0-%D0%B2%D0%B5%D1%80%D1%82%D0%BE%D0%BB%D0%B5%D1%82-%D0%BC%D0%B8%D0%BC%D0%B8%D0%BC%D0%B8-%D0%91%D0%B0%D1%8F%D0%BD-908995.jpeg';
+    teleBot.sendPhoto(id, url, {
+      fileName: 'mimimi.jpg',
+      serverDownload: true
+    })
+    teleBot.sendAction(id, 'upload_photo')
+  }
 
   if (text.indexOf('запомните этот твит') > -1) {
     jokes.rememberTweet(msg, teleBot)
