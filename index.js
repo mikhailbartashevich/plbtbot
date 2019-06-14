f5f490ba5155ad3294022e219ed163cc197be64c
Mikhail <mihailbarashevich@gmail.com>
2019-06-14 10:33:35 +0200

    removed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-require('newrelic')
-const TeleBot = require('telebot')
-const bot = new TeleBot(process.env.BOT_ID)
-const botRoutes = require('./src/bot-routes')
 const httpServer = require('./src/http')
 
 botRoutes.initRoutes(bot)
